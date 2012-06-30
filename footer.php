c71d6a7641e410770562c64fe4a7b9eb740b05ec
Brian Nani <brian@codeforart.com>
2012-06-30 16:00:35 -0700

    Added References Folder and Images

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
 
 <footer>
     <p id="copyright">
-    &copy; <?php echo date("Y") ?> <?php bloginfo( 'name' ) ?>. All Rights Reserved. Web Solution Provided by <a href="http://codeforart.com/">Code For Art Studio</a>
+    &copy; <?php echo date("Y") ?> <?php bloginfo( 'name' ) ?>. All Rights Reserved. 
     </p>
 </footer>
 
