b13cbc21750bcedaf4269bc2b9c6a7f1d85d74e3
Brian Nani <brian@codeforart.com>
2012-07-01 23:15:16 -0700

    Last Update Before Presentation

--- single-artwork.php
+++ single-artwork.php
@@ -1,5 +1,5 @@
 <?php get_header(); ?>
-<article id="content">
+<article id="content" class="grid_8">
     <?php the_post(); ?>
     <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
         <h1 class="entry-title"><?php the_title(); ?></h1>
@@ -9,9 +9,14 @@
             
             $artwork_availability = get_post_meta( $post->ID, 'artwork_availability', true);
             $artwork_price        = get_post_meta( $post->ID, 'artwork_price', true);
-            echo do_shortcode('[nicepaypallite amount="'.$artwork_price.'" name="'.get_the_title().'"]');
-                
+            $artwork_price = '$'.number_format($artwork_price, 2);
             ?>
+            
+            <div class="artwork price">Price: <?php echo $artwork_price ?></div>
+            <?php
+            echo do_shortcode('[nicepaypallite amount="'.$artwork_price.'" name="'.get_the_title().'"]');
+            ?>
+            
             <?php wp_link_pages('before=<div class="page-link">' . __( 'Pages:', 'Team4' ) . '&after=</div>') ?>
             <?php edit_post_link( __( 'Edit', 'Team4' ), '<span class="edit-link">', '</span>' ) ?>
 
